04c0b3021ea99734ff78012dfc90a0acad94da75
Dave Whipp <david.whipp@helsinki.fi>
2022-01-27 20:15:31 +0200

    Tweak environment to try to get it to build

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ author = 'David Whipp'
 extensions = [
     #"sphinx.ext.autodoc",
     #"sphinx.ext.napoleon",
-    #"IPython.sphinxext.ipython_console_highlighting",
-    #"IPython.sphinxext.ipython_directive",
-    #"myst_nb",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+    "myst_nb",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,21 +40,19 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_book_theme'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # HTML theme options
 html_theme_options = {
