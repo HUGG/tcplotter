cb2bf742b488c3efe8c58a737289458a978defd2
Dave Whipp <david.whipp@helsinki.fi>
2025-03-18 09:33:14 +0200

    Bump version number to 0.3.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tcplotter',
-      version='0.3.9',
+      version='0.3.10',
       description='Plots thermochronometer ages and closure temperatures',
       url='https://github.com/HUGG/tcplotter',
       author='David Whipp',
